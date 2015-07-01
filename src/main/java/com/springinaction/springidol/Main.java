f18fc74ec8c1be8d8c004ec5a6b17ea93c03d1d8
Sergey Petrov <spetrov@vseplatezhi.ru>
2015-07-01 11:20:21 +0600

    init repo

---
+++ src/main/java/com/springinaction/springidol/Main.java
@@ -0,0 +1,16 @@
+package com.springinaction.springidol;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by p12se on 29.06.15.
+ */
+public class Main {
+    public static void main(String[] args) throws PerformanceException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springinaction/springidol/spring-idol.xml");
+
+        Performer performer = (Performer) context.getBean("kenny");
+        performer.perform();
+    }
+}
