7ee703b4e2f01f16ab5f678973d4baf00adb9711
p12se <p12se@ya.ru>
2015-07-17 20:58:52 +0600

    by trying several pointcuts in one aspect

--- src/main/java/com/springinaction/springidol/Audience.java
+++ src/main/java/com/springinaction/springidol/Audience.java
@@ -1,5 +1,7 @@
 package com.springinaction.springidol;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * Created by p12se on 17.07.15.
  */
@@ -16,5 +18,26 @@ public class Audience {
     }
     public void demandRefund(){
         System.out.println("Boo WE want our money back!");
+    }
+
+    public void ura(){
+        System.out.println("URĀÆ URĀÆ URĀÆ URĀÆ URĀÆ URĀÆ URĀÆ URĀÆ URĀÆ URĀÆ");
+    }
+
+    public void watchPerformance(ProceedingJoinPoint joinPoint){
+        try{
+            System.out.println("The audience is taking their seats.");
+            System.out.println("The audience is turning off their cellphones");
+            long start = System.currentTimeMillis();
+
+            joinPoint.proceed();
+
+            long end = System.currentTimeMillis();
+            System.out.println("CLAP CLAP CLAP CLAP");
+            System.out.println("The performance took " + (end - start) + " milliseconds");
+
+        }catch (Throwable t){
+            System.out.println("BOOO We want our money back!!!!!");
+        }
     }
 }
